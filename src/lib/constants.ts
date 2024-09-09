c0c1d204b3fb8b8d7755cbaafcc5287aca868e16
Always Ritz <161363790+Ritz404@users.noreply.github.com>
2024-09-10 01:13:00 +0700

    Add files via upload

---
+++ src/lib/constants.ts
@@ -0,0 +1,49 @@
+export const data = [
+  {
+    text: 'Sensei! seseorang memberikan surat ini untukmu. Aku bacakan ya?',
+    audio: '/audio/birthday/ask-for-read.wav',
+    delay: 0,
+    choices: [
+      {
+        text: 'Ya, bacakan untukku Yuuka!',
+        selected: false
+      }
+    ]
+  },
+  {
+    text: 'Dia bilang... "Selamat ulang tahun yang ke-17, Ritz! Semoga hari ini kamu dalam kondisi yang sehat dan berbahagia ya!"',
+    audio: '/audio/birthday/hbd-1-2.wav',
+    delay: 1200,
+    choices: []
+  },
+  {
+    text: '"Semoga di tahun ini kamu diberi kesehatan, kebahagiaan dan semoga lancar dalam Pembelajaran, kamu!"',
+    audio: '/audio/birthday/hbd-2.wav',
+    delay: 500,
+    choices: []
+  },
+  {
+    text: '"Dan semoga cita-cita kamu cepat tercapai ya! dan semoga berkah untuk umur kamu yang ke-17 ini!"',
+    audio: '/audio/birthday/hbd-3.wav',
+    delay: 500,
+    choices: []
+  },
+  {
+    text: '"Dan... semoga kamu makin Ganteng dan manis ya! ehehe."',
+    audio: '/audio/birthday/hbd-4-2.wav',
+    delay: 800,
+    choices: []
+  },
+  {
+    text: '"Teman tercinta kamu, Shiroko, sekali lagi mengucapkan selamat ulang tahun padamu!"',
+    audio: '/audio/birthday/hbd-5.wav',
+    delay: 500,
+    choices: []
+  },
+  {
+    text: '"-- dari Shiroko Istrimu."',
+    audio: '/audio/birthday/epilogue.wav',
+    delay: 500,
+    choices: []
+  }
+];
